95fb48e7f16aaae44ca79547f5325e881a842da4
vewadie <133230074+vewadie@users.noreply.github.com>
2023-11-07 18:13:45 -0300

    contraste primeira parte

---
+++ PI-MissaoAmbiental/scripts/contraste.js
@@ -0,0 +1,8 @@
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const chk = document.getElementById('chk');
+    chk.addEventListener('change', () => {
+      document.body.classList.toggle('dark');
+    });
+  });
+  
