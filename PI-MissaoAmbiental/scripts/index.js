39ad78767eb51f08b7f73511afa4010596d31869
Rodrigo <23.00068-6@maua.br>
2023-11-09 19:19:53 -0300

    home buttons e veja menos animation

--- PI-MissaoAmbiental/scripts/index.js
+++ PI-MissaoAmbiental/scripts/index.js
@@ -1,11 +1,24 @@
+var posicaoOriginal;
+
 document.getElementById('veja-mais').addEventListener('click', function() {
     var conteudoOculto = document.querySelector('.conteudo-oculto');
 
     if (conteudoOculto.classList.contains('mostrar')) {
         conteudoOculto.classList.remove('mostrar');
         this.innerHTML = 'Veja Mais';
+
+        // Rolar de volta para a posição original
+        window.scrollTo(0, posicaoOriginal);
     } else {
         conteudoOculto.classList.add('mostrar');
         this.innerHTML = 'Veja Menos';
+
+        // Armazenar a posição original antes de expandir o conteúdo
+        posicaoOriginal = window.pageYOffset;
     }
 });
+
+
+
+
+
