d8903782a75bca4f1d83df89f8a7800c0a987678
vewadie <133230074+vewadie@users.noreply.github.com>
2023-11-16 16:28:51 -0300

    contraste final + botoes e fonte home + design agendamento + design nossa historia + padding footers

--- PI-MissaoAmbiental/scripts/contraste.js
+++ PI-MissaoAmbiental/scripts/contraste.js
@@ -6,3 +6,28 @@
     });
   });
   
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const chk = document.getElementById('chk');
+    
+    // Verifica se há preferência salva
+    const modo = localStorage.getItem('modoContraste');
+  
+    // Aplica o tema correspondente
+    if (modo === 'dark') {
+      document.body.classList.add('dark');
+      chk.checked = true; // Marcando o checkbox se o modo dark estiver ativo
+    }
+  
+    // Alterna o modo e salva a preferência
+    chk.addEventListener('change', () => {
+      if (chk.checked) {
+        document.body.classList.add('dark');
+        localStorage.setItem('modoContraste', 'dark');
+      } else {
+        document.body.classList.remove('dark');
+        localStorage.setItem('modoContraste', 'light'); // Opcionalmente, você pode salvar um modo 'light'
+      }
+    });
+  });
+  
